cd14578d9ea5a6b83aef384710097c753bd2377f
mjmtg <thisismjmoshiri@gmail.com>
2022-01-29 15:40:48 +0200

    Made Input Statement more clear, added a variable for setting the number of blocks.

--- Main.java
+++ Main.java
@@ -9,14 +9,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         BlockChain chain;
+        int blockCounter;
         File file = new File("BlockChain");
         if (file.exists() && !file.isDirectory())
             chain = new BlockChain("BlockChain");
         else {
-            System.out.println("Enter the number of Zeroes: ");
+            System.out.println("Enter the number of Zeroes you want at beginning of each hash: ");
             chain = new BlockChain(scanner.nextInt());
-            for (int i = 0; i < 5; i++) {
+            System.out.println("How Many Blocks Do you want to create ? : ");
+            blockCounter = scanner.nextInt() + 1;
+            for (int i = 1; i < blockCounter; i++) {
+                System.out.println("Creating Block" + i);
                 chain.addBlock();
+                System.out.println("Block " + i + " Created");
             }
             SerializationUtils.WriterCloser();
         }
